5455869d653e4351683bad3b4d41b8b5359e6611
angeladro <angela.drontschenko@gmail.com>
2025-06-01 19:50:38 +0200

    Tf-file modified

--- project_MTVML/terraform/main.tf
+++ project_MTVML/terraform/main.tf
@@ -74,12 +74,36 @@ resource "aws_security_group" "main_sg" {
   }
 
   ingress {
+  description = "RabbitMQ Web UI"
+  from_port   = 15672
+  to_port     = 15672
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+  ingress {
     description = "Allow Memcached"
     from_port   = 11211
     to_port     = 11211
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+  description = "Allow MySQL"
+  from_port   = 3306
+  to_port     = 3306
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+  description = "Allow Tomcat"
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
   egress {
     from_port   = 0
