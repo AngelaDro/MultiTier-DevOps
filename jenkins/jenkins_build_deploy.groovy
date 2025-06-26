8aed600822d92403f2f740fb19ee76a5e912a2f3
angeladro <angela.drontschenko@gmail.com>
2025-06-26 20:47:24 +0200

    jenkins file changed

--- jenkins/jenkins_build_deploy.groovy
+++ jenkins/jenkins_build_deploy.groovy
@@ -10,7 +10,7 @@ pipeline {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-terraform-access']]) {
                     script {
                         env.ACCOUNT_ID = sh(script: 'aws sts get-caller-identity --query Account --output text', returnStdout: true).trim()
-                        env.ECR_REPO = "${env.ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${IMAGE_NAME}"
+                        env.ECR_REPO = "${env.ACCOUNT_ID}.dkr.ecr.${env.AWS_REGION}.amazonaws.com/${env.IMAGE_NAME}"
                         env.IMAGE_TAG = env.BUILD_NUMBER
                     }
                 }
@@ -19,34 +19,38 @@ pipeline {
         stage('Build Docker Image') {
             steps {
                 script {
-                    docker.build("my-java-app:${env.IMAGE_TAG}", "-f jenkins/app.Dockerfile .")
+                    docker.build("my-java-app:${env.IMAGE_TAG}", "-f app.Dockerfile .")
                 }
             }
         }
         stage('Push to AWS ECR') {
             steps {
-                sh '''
-                    aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REPO
-                    docker tag my-java-app:${IMAGE_TAG} $ECR_REPO:${IMAGE_TAG}
-                    docker push $ECR_REPO:${IMAGE_TAG}
-                '''
+                script {
+                    sh """
+                        aws ecr get-login-password --region ${env.AWS_REGION} | docker login --username AWS --password-stdin ${env.ECR_REPO}
+                        docker tag my-java-app:${env.IMAGE_TAG} ${env.ECR_REPO}:${env.IMAGE_TAG}
+                        docker push ${env.ECR_REPO}:${env.IMAGE_TAG}
+                    """
+                }
             }
         }
         stage('Deploy with Ansible') {
             steps {
                 sshagent(['jenkins-ansible-credentials-id']) {
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-terraform-access']]) {
-                withEnv([
-                    "AWS_ACCESS_KEY_ID=${env.AWS_ACCESS_KEY_ID}",
-                    "AWS_SECRET_ACCESS_KEY=${env.AWS_SECRET_ACCESS_KEY}",
-                    "AWS_DEFAULT_REGION=${env.AWS_REGION}"
-                    ]) {
-                    sh '''
-                        ansible-playbook -i ansible/hosts ansible/deploy.yml \
-                            -u ubuntu \
-                            --extra-vars "image_tag=${IMAGE_TAG} ecr_repo=${ECR_REPO}" \
-                            --ssh-extra-args='-o StrictHostKeyChecking=no'
-                    '''
+                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-terraform-access']]) {
+                        withEnv([
+                            "AWS_ACCESS_KEY_ID=${env.AWS_ACCESS_KEY_ID}",
+                            "AWS_SECRET_ACCESS_KEY=${env.AWS_SECRET_ACCESS_KEY}",
+                            "AWS_DEFAULT_REGION=${env.AWS_REGION}"
+                        ]) {
+                            sh """
+                                ansible-playbook -i ansible/hosts ansible/deploy.yml \
+                                    -u ubuntu \
+                                    --extra-vars "image_tag=${env.IMAGE_TAG} ecr_repo=${env.ECR_REPO}" \
+                                    --ssh-extra-args='-o StrictHostKeyChecking=no'
+                            """
+                        }
+                    }
                 }
             }
         }
