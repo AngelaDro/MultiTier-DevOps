c0cfef777659b5a2d8fb64d95dcee42fb57f56ff
angeladro <angela.drontschenko@gmail.com>
2025-06-30 21:22:06 +0200

    change

--- jenkins/jenkins_build_deploy.groovy
+++ jenkins/jenkins_build_deploy.groovy
@@ -10,13 +10,7 @@ pipeline {
                 git branch: 'vpro-file', url: 'https://github.com/AngelaDro/MultiTier-DevOps.git'
             }
         }
-        stage('Debug workspace') {
-            steps {
-                sh 'pwd'
-                sh 'ls -la'
-                sh 'ls -la jenkins || echo "No jenkins directory found"'
-            }
-        }
+        
         stage('Get AWS Account ID') {
             steps {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-terraform-access']]) {
@@ -46,22 +40,42 @@ pipeline {
                 }
             }
         }
+        
+        stage('Update Ansible Hosts') {
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-terraform-access']]) {
+                    script {
+                        def tomcat_ip = sh(
+                            script: '''
+                                aws ec2 describe-instances \
+                                    --filters "Name=tag:Name,Values=tomcat" "Name=instance-state-name,Values=running" \
+                                    --query "Reservations[*].Instances[*].PublicIpAddress" \
+                                    --output text
+                            ''',
+                            returnStdout: true
+                        ).trim()
+
+                        echo "Tomcat IP: ${tomcat_ip}"
+
+                        writeFile file: 'ansible/hosts', text: """
+[tomcat]
+${tomcat_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/DevopsCourseKeys.pem
+"""
+                    }
+                }
+            }
+        }
+        
         stage('Deploy with Ansible') {
             steps {
-                sshagent(['jenkins-ansible-credentials-id']) {
+                sshagent(['aws_devopscourse_key']) {
                     withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-terraform-access']]) {
-                        withEnv([
-                            "AWS_ACCESS_KEY_ID=${env.AWS_ACCESS_KEY_ID}",
-                            "AWS_SECRET_ACCESS_KEY=${env.AWS_SECRET_ACCESS_KEY}",
-                            "AWS_DEFAULT_REGION=${env.AWS_REGION}"
-                        ]) {
-                            sh """
-                                ansible-playbook -i ansible/hosts ansible/deploy.yml \
-                                    -u ubuntu \
-                                    --extra-vars "image_tag=${env.IMAGE_TAG} ecr_repo=${env.ECR_REPO}" \
-                                    --ssh-extra-args='-o StrictHostKeyChecking=no'
-                            """
-                        }
+                        sh """
+                            ansible-playbook -i ansible/hosts ansible/deploy.yml \
+                                -u ubuntu \
+                                --extra-vars "image_tag=${env.IMAGE_TAG} ecr_repo=${env.ECR_REPO} aws_access_key=${env.AWS_ACCESS_KEY_ID} aws_secret_key=${env.AWS_SECRET_ACCESS_KEY}" \
+                                --ssh-extra-args='-o StrictHostKeyChecking=no'
+                        """
                     }
                 }
             }
